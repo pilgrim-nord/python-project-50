57795727657e3f3bb175afe55dc0b9f935176c3e
pilgrim-nord <dmelekhov@gmail.com>
2025-06-27 12:36:12 +0300

    small changes

--- gendiff/make_diff.py
+++ gendiff/make_diff.py
@@ -4,13 +4,10 @@ from gendiff.input_data import input_data
 def generate_diff(path_file1, path_file2):
     file1 = input_data(path_file1)
     file2 = input_data(path_file2)
-    #
-    # sorted_1 = dict(sorted(file1.items()))
-    # sorted_2 = dict(sorted(file2.items()))
+
     keys = sorted(file1.keys() | file2.keys())
     diff_lines = []
-    # diff_string = f'{{\n'
-    #
+
     for key in keys:
         val1 = file1.get(key)
         val2 = file2.get(key)
