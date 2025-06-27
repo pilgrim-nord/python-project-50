737de2b409dc9c44abf264a19e06c40180df6aab
pilgrim-nord <dmelekhov@gmail.com>
2025-06-27 17:57:42 +0300

    test cov edit

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,9 +2,15 @@ from pathlib import Path
 from gendiff.make_diff import generate_diff
 
 
-def test_generate_diff():
-    # Путь к директории с фикстурами
-    fixtures_dir = Path(__file__).parent / "fixtures"
+# Ожидаемый результат
+
+def test_generate_diff_json():
+
+    fixtures_dir = Path(__file__).parent / "test_data"
+
+    expected_file_path = fixtures_dir / "result.txt"
+    with open(expected_file_path, "r", encoding="utf-8") as file:
+        expected = file.read()
 
     # Пути к файлам
     file1_path = fixtures_dir / "file1.json"
@@ -13,21 +19,17 @@ def test_generate_diff():
     # Вызываем тестируемую функцию
     result = generate_diff(str(file1_path), str(file2_path))
 
-    # Ожидаемый результат
-    expected = """{
-- follow: False
-  host: hexlet.io
-- proxy: 123.234.53.22
-- timeout: 50
-+ timeout: 20
-+ verbose: True
-}"""
+
 
     assert result == expected
 
 def test_generate_diff_yaml():
-    # Путь к директории с фикстурами
-    fixtures_dir = Path(__file__).parent / "fixtures"
+
+    fixtures_dir = Path(__file__).parent / "test_data"
+
+    expected_file_path = fixtures_dir / "result.txt"
+    with open(expected_file_path, "r", encoding="utf-8") as file:
+        expected = file.read()
 
     # Пути к файлам
     file1_path = fixtures_dir / "file1.yaml"
@@ -36,14 +38,5 @@ def test_generate_diff_yaml():
     # Вызываем тестируемую функцию
     result = generate_diff(str(file1_path), str(file2_path))
 
-    # Ожидаемый результат
-    expected = """{
-- follow: False
-  host: hexlet.io
-- proxy: 123.234.53.22
-- timeout: 50
-+ timeout: 20
-+ verbose: True
-}"""
 
     assert result == expected
