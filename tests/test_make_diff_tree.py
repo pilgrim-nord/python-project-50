1061b56df012e3b56830ec1854ee3d350f821b3c
pilgrim-nord <dmelekhov@gmail.com>
2025-07-25 15:20:10 +0300

    correct test

--- tests/test_make_diff_tree.py
+++ tests/test_make_diff_tree.py
@@ -1,12 +1,23 @@
 import pprint
 from pathlib import Path
 from gendiff.diff_tree import make_diff_tree
+from gendiff.generate import generate_diff
 from gendiff.input_data import input_data
 from gendiff.formatter.stylish import stylish
 from gendiff.formatter.plain import plain
 from gendiff.formatter.json import json
 
 test_data_dir = Path(__file__).parent / "test_data"
+
+def test_generate_diff():
+    expected_file_path = test_data_dir / "diff_result_stylish.txt"
+    with open(expected_file_path, "r") as file:
+        expected = file.read()
+    file_path1 = test_data_dir / "file1.json"
+    file_path2 = test_data_dir / "file2.json"
+    diff_default = generate_diff(file_path1, file_path2)
+
+    assert diff_default == expected
 
 def test_make_diff_tree_json_stylish():
 
